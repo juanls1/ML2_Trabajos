cf1e6a23398826dd31cbb23c22892bc431487983
juanls1 <202308780@alu.comillas.edu>
2024-03-26 17:50:47 +0100

    changes (necessary to fix last cell)

--- 2nd project/local_functs.py
+++ 2nd project/local_functs.py
@@ -1,6 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Visualize a few images
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
 # Función para mostrar una cuadrícula de imágenes con títulos
 def show_images_grid(images, titles, rows, cols):
     fig, axes = plt.subplots(rows, cols, figsize=(15, 15))
