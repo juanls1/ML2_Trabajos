9ddb50759f2edd04a3d98de1f65bf32b226c40d6
juanls1 <202308780@alu.comillas.edu>
2024-04-03 17:52:53 +0200

    last constants changes

--- 2nd project/config/constants.py
+++ 2nd project/config/constants.py
@@ -1,15 +1,35 @@
 # Constants necessary for the project
 
-# Constants used for displaying a random sample of images in the notebook
+# Constants used for displaying a random sample of images in the notebook for training
 Number_random_images = 64
 Number_rows = 8
 Number_cols = 8
 Figsize = (15, 15)
 Fontsize = 8
 
+# Constants used for displaying a random sample of images in the notebook for validation
+Number_random_images_val = 64
+Number_rows_val = 8
+Number_cols_val = 8
+Figsize = (15, 15)
+Fontsize = 8
+
+
 # Constants used for loading the data (they affect the performance and functioning of the model)
 Batch_size = 32 
 Images_size = 224   # ResNet50 requires 224x224 images
+
+# Constants used for the model
+Model_used = 'resnet50'
+Learning_rate = 1e-4
+Number_epochs = 1
+Criterion = 'cross_entropy'
+Optimizer = 'Adam'
+Model_name = 'resnet50-1epoch' # Name of the model to save
+
+# Constants used for the prediction
+Model_loaded = 'resnet50-1epoch' # Name of the model to load
+Model_used_load = 'resnet50' # Model used for the loaded model
 
 # Constants used for the streamlit app
 Images_types = ['png', 'jpg', 'jpeg']
