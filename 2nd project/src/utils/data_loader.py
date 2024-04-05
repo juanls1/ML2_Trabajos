63598c9c0e2ef8960e1a8e88cad394b7fa446c52
juanls1 <202308780@alu.comillas.edu>
2024-04-05 15:28:54 +0200

    last changes

--- 2nd project/src/utils/data_loader.py
+++ 2nd project/src/utils/data_loader.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import os
 
 # Obtener la ruta absoluta de la carpeta que contiene el módulo
 root_dir = Path(__file__).resolve().parent.parent.parent
@@ -11,8 +12,8 @@ from src.utils.cnn import load_data
 from config.constants import Batch_size, Images_size
 
 # Load data 
-train_dir = 'C:/Users/juanl/Documents/Master/2o cuatri/ML2/Codes/Entregas/ML2_Trabajos/2nd project/data/training'
-valid_dir = 'C:/Users/juanl/Documents/Master/2o cuatri/ML2/Codes/Entregas/ML2_Trabajos/2nd project/data/validation'
+train_dir = os.path.join(root_dir, 'data', 'training')
+valid_dir = os.path.join(root_dir, 'data', 'validation')
 
 train_loader, valid_loader, num_classes = load_data(train_dir, 
                                                     valid_dir, 
